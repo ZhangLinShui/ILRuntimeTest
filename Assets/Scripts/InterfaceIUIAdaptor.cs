74257a7af313c67880feb3409e2eea5df5934657
ZhangLinShui <linshuizhang@gmail.com>
2020-05-20 22:13:35 +0800

    添加项目文件。

---
+++ Assets/Scripts/InterfaceIUIAdaptor.cs
@@ -0,0 +1,149 @@
+﻿using ILRuntime.CLR.Method;
+using ILRuntime.Runtime.Enviorment;
+using ILRuntime.Runtime.Intepreter;
+using System;
+using static CoroutineAdapter;
+
+public class InterfaceIUIAdaptor : CrossBindingAdaptor
+{
+
+    //public override Type BaseCLRType
+    //{
+    //    get
+    //    {
+    //        return typeof(IUI);//这是你想继承的那个类
+    //    }
+    //}
+
+    //public override Type AdaptorType
+    //{
+    //    get
+    //    {
+    //        return typeof(Adaptor);//这是实际的适配器类
+    //    }
+    //}
+
+    //public override object CreateCLRInstance(ILRuntime.Runtime.Enviorment.AppDomain appdomain, ILTypeInstance instance)
+    //{
+    //    return new Adaptor(appdomain, instance);//创建一个新的实例
+    //}
+
+    ////实际的适配器类需要继承你想继承的那个类，并且实现CrossBindingAdaptorType接口
+    //public class Adaptor : IUI, CrossBindingAdaptorType
+    //{
+    //    ILTypeInstance instance;
+    //    ILRuntime.Runtime.Enviorment.AppDomain appdomain;
+
+    //    IMethod m_Start;
+    //    bool m_StartGot;
+
+    //    IMethod m_Update;
+    //    bool m_UpdateGot;
+
+    //    public Adaptor()
+    //    {
+
+    //    }
+
+    //    public Adaptor(ILRuntime.Runtime.Enviorment.AppDomain appdomain, ILTypeInstance instance)
+    //    {
+    //        this.appdomain = appdomain;
+    //        this.instance = instance;
+    //    }
+
+    //    public ILTypeInstance ILInstance { get { return instance; } }
+
+    //    //你需要重写所有你希望在热更脚本里面重写的方法，并且将控制权转到脚本里去
+    //    public void Start()
+    //    {
+    //        if (!m_StartGot)
+    //        {
+    //            m_Start = instance.Type.GetMethod("Start", 0);
+    //            m_StartGot = true;
+    //        }
+    //        if (m_Start != null)
+    //        {
+    //            appdomain.Invoke(m_Start, instance, null);//没有参数建议显式传递null为参数列表，否则会自动new object[0]导致GC Alloc
+    //        }
+    //    }
+
+    //    public void Update()
+    //    {
+    //        if (!m_UpdateGot)
+    //        {
+    //            m_Update = instance.Type.GetMethod("Update", 0);
+    //            m_UpdateGot = true;
+    //        }
+    //        if (m_Update != null)
+    //        {
+    //            appdomain.Invoke(m_Update, instance, null);
+    //        }
+    //    }
+    //}
+
+    //————————————————
+    //版权声明：本文为CSDN博主「王王王渣渣」的原创文章，遵循CC 4.0 BY-SA版权协议，转载请附上原文出处链接及本声明。
+    //原文链接：https://blog.csdn.net/wangjiangrong/java/article/details/90294366
+    public override Type BaseCLRType => typeof(IUI);//这是你想继承的那个类
+
+
+    public override Type AdaptorType => typeof(Adaptor);//这是实际的适配器类
+
+
+    public override object CreateCLRInstance(ILRuntime.Runtime.Enviorment.AppDomain appdomain, ILTypeInstance instance)
+    {
+        return new Adaptor(appdomain, instance);//创建一个新的实例
+
+    }//实际的适配器类需要继承你想继承的那个类，并且实现CrossBindingAdaptorType接口
+    public class Adaptor : IUI, CrossBindingAdaptorType
+    {
+        ILTypeInstance instance;
+        ILRuntime.Runtime.Enviorment.AppDomain appdomain;
+
+        IMethod m_Start;
+        bool m_StartGot;
+
+        IMethod m_Update;
+        bool m_UpdateGot;
+
+        public Adaptor()
+        {
+
+        }
+
+        public Adaptor(ILRuntime.Runtime.Enviorment.AppDomain appdomain, ILTypeInstance instance)
+        {
+            this.appdomain = appdomain;
+            this.instance = instance;
+        }
+
+        public ILTypeInstance ILInstance { get { return instance; } }
+
+        //你需要重写所有你希望在热更脚本里面重写的方法，并且将控制权转到脚本里去
+        public void Start()
+        {
+            if (!m_StartGot)
+            {
+                m_Start = instance.Type.GetMethod("Start", 0);
+                m_StartGot = true;
+            }
+            if (m_Start != null)
+            {
+                appdomain.Invoke(m_Start, instance, null);//没有参数建议显式传递null为参数列表，否则会自动new object[0]导致GC Alloc
+            }
+        }
+
+        public void Update()
+        {
+            if (!m_UpdateGot)
+            {
+                m_Update = instance.Type.GetMethod("Update", 0);
+                m_UpdateGot = true;
+            }
+            if (m_Update != null)
+            {
+                appdomain.Invoke(m_Update, instance, null);
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/Launch.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using UnityEngine;
+
+public class Launch : MonoBehaviour
+{
+    List<Action> DllUIUpdateList = new List<Action>();
+
+    ILRuntime.Runtime.Enviorment.AppDomain appdomain;
+    void Start()
+    {
+        
+        StartCoroutine(LoadILRuntime());
+    }
+   
+
+    IEnumerator LoadILRuntime()
+    {
+        //读取dll文件
+        appdomain = new ILRuntime.Runtime.Enviorment.AppDomain();
+
+        WWW www = new WWW(Application.streamingAssetsPath + "/Hotfix.dll");
+
+        while (!www.isDone)
+        {
+            yield return null;
+        }
+        if (!string.IsNullOrEmpty(www.error))
+        {
+            Debug.LogError(www.error);
+        }
+        byte[] dll = www.bytes;
+        www.Dispose();
+
+        www = new WWW(Application.streamingAssetsPath + "/Hotfix.pdb");
+
+        while (!www.isDone)
+        {
+            yield return null;
+        }
+        if (!string.IsNullOrEmpty(www.error))
+        {
+            Debug.LogError(www.error);
+        }
+        byte[] pdb = www.bytes;
+        //这个流 只加载用到 的部分 意思是流要保持全局变量 且需动态加载
+        System.IO.MemoryStream fs = new MemoryStream(dll);
+        System.IO.MemoryStream p = new MemoryStream(pdb);
+        appdomain.LoadAssembly(fs, p, new ILRuntime.Mono.Cecil.Pdb.PdbReaderProvider());
+
+        appdomain.DebugService.StartDebugService(56000);
+        //StartCoroutine(WaitForZhiZhang());
+        OnILRuntimeInit();
+        OnILRuntimeInitialized();
+    }
+    IEnumerator WaitForZhiZhang()
+    {
+        while (!appdomain.DebugService.IsDebuggerAttached){
+            yield return null;
+        }
+        yield return new WaitForSeconds(1);
+        OnILRuntimeInit();
+        OnILRuntimeInitialized();
+    }
+
+    void Update()
+    {
+        Debug.Log("开始执行主工程Update函数");
+        if (DllUIUpdateList.Count > 0)
+        {
+            for(int i=0;i< DllUIUpdateList.Count; i++)
+            {
+                DllUIUpdateList[i].Invoke();
+            }
+        }
+    }
+
+    void OnILRuntimeInit()
+    {
+        //跨域继承绑定适配器
+        appdomain.RegisterCrossBindingAdaptor(new InterfaceIUIAdaptor());
+        //Button点击事件的委托注册
+        appdomain.DelegateManager.RegisterDelegateConvertor<UnityEngine.Events.UnityAction>((act) =>
+        {
+            return new UnityEngine.Events.UnityAction(() =>
+            {
+                ((Action)act)();
+            });
+        });
+    }
+
+    void OnILRuntimeInitialized()
+    {
+        
+        //获取Hotfix.dll内部定义的类
+        List<Type> allTypes = new List<Type>();
+        var values = appdomain.LoadedTypes.Values.ToList();
+        foreach (var v in values)
+        {
+            allTypes.Add(v.ReflectionType);
+        }
+        //去重
+        allTypes = allTypes.Distinct().ToList();
+
+        DllUIUpdateList.Clear();
+        foreach (var v in allTypes)
+        {
+            //找到实现IUI接口的类 Adaptor 前面写的适配器IUI的类
+            if (v.IsClass && v.GetInterface("Adaptor") != null)
+            {
+                //生成实例
+                var gs = appdomain.Instantiate<IUI>(v.FullName);
+
+                //调用接口方法
+                gs.Start();
+                DllUIUpdateList.Add(gs.Update);
+            }
+        }
+    }
+}
+
